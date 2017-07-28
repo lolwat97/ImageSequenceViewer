b740a2526aa7744bdf69742028f5e6cb122e782d
Sergey Sofeychuk <lolwat97@gmail.com>
2017-07-28 15:39:13 +0300

    PEP8 compliancy

--- viewer.py
+++ viewer.py
@@ -3,93 +3,91 @@ from PIL import Image, ImageTk
 import os
 import pickle
 
+
 class Viewer:
     def __init__(self):
         self.path = 'img'
-        self.thumbSize = (200, 200) # максимальный размер тамбнейликов
-        self.previewSize = (500,500) # максимальный размер превьюшечки
-        self.root = Tk() # создаем главное окно
-        self.imageThumbnailFrame = Frame(self.root) # фрейм для тамбнейликов
-        self.imagePreviewFrame = Frame(self.root) # фрейм для превьюшки
+        self.thumbSize = (200, 200)  # максимальный размер тамбнейликов
+        self.previewSize = (500, 500)  # максимальный размер превьюшечки
+        self.root = Tk()  # создаем главное окно
+        self.imageThumbnailFrame = Frame(self.root)  # фрейм для тамбнейликов
+        self.imagePreviewFrame = Frame(self.root)  # фрейм для превьюшки
         self.previewLabel = Label(self.imagePreviewFrame)
         self.prevClick = ''
         self.nowClick = ''
         # сохраняем пути картинок всех
         self.imageFilenames = [os.path.join(self.path, file) for file in os.listdir(self.path)]
         # грузим картинки в Label-объекты, привязанные к фрейму для тамбнейликов
-        
 
-        self.imageBasenames = pickle.load( open("order.pic","rb"))
+        self.imageBasenames = pickle.load(open("order.pic", "rb"))
 
         if self.imageFilenames == self.imageBasenames:
             print("Nothing new")
             self.imageFilenames = self.imageBasenames
         else:
             print("New or deleted images")
-            #if new найти и добавить новое изображение в конец бд
+            # if new найти и добавить новое изображение в конец бд
             for simage in self.imageFilenames:
                 if simage in self.imageBasenames:
                     pass
                 else:
                     print("New one: ", simage)
-                    self.imageBasenames.append(simage) #
-            #if deleted удалить из бд, удаленное
+                    self.imageBasenames.append(simage)
+            # if deleted удалить из бд, удаленное
             for bimage in self.imageBasenames:
                 if bimage in self.imageFilenames:
                     pass
                 else:
-                    print("Deleted one: ", bimage)            
-                    self.imageBasenames.remove(bimage) #
+                    print("Deleted one: ", bimage)
+                    self.imageBasenames.remove(bimage)
             self.imageFilenames = self.imageBasenames
-        #pickle.dump(self.imageFilenames, open("order.pic","wb"))
+        # pickle.dump(self.imageFilenames, open("order.pic","wb"))
 
-        
-        self.imageThumbnailLabels = self.makeImageLabels(self.imageThumbnailFrame, self.imageFilenames, self.thumbSize)
+        self.imageThumbnailLabels = self.make_image_labels(self.imageThumbnailFrame, self.imageFilenames,
+                                                           self.thumbSize)
 
-        self.setImagePreviewLabel(self.imagePreviewFrame, self.imageFilenames[0], self.previewSize)
+        self.set_image_preview_label(self.imagePreviewFrame, self.imageFilenames[0], self.previewSize)
 
-        self.previewLabel.pack() # запихиваем превьюшку в imagePreview
+        self.previewLabel.pack()  # запихиваем превьюшку в imagePreview
         self.previewLabel.name = "preview"
         self.previewLabel.bind("<1>", self.on_main_click)
-        self.packFrames() # запихиваем фреймы в главное окно
-        self.packImages(self.imageThumbnailLabels) # запихиваем тамбнейлики в imageGrid
+        self.pack_frames()  # запихиваем фреймы в главное окно
+        self.pack_images(self.imageThumbnailLabels)  # запихиваем тамбнейлики в imageGrid
 
-        
-    def setImagePreviewLabel(self, window, file, size): # ставим картинку в Label для превью
-        image = Image.open(file) # открываем картинку с помощью PIL
-        image.thumbnail(size, Image.ANTIALIAS) # Уменьшаем ее до размера size
-        photoimage = ImageTk.PhotoImage(image) # Конвертируем картинку в формат TkInter
-        self.previewLabel = Label(window, image=photoimage) # Делаем Label
-        self.previewLabel.image = photoimage # Сохраняем референс (вот это глупо сделано, как по-моему, но так надо)
+    def set_image_preview_label(self, window, file, size):  # ставим картинку в Label для превью
+        image = Image.open(file)  # открываем картинку с помощью PIL
+        image.thumbnail(size, Image.ANTIALIAS)  # Уменьшаем ее до размера size
+        photoimage = ImageTk.PhotoImage(image)  # Конвертируем картинку в формат TkInter
+        self.previewLabel = Label(window, image=photoimage)  # Делаем Label
+        self.previewLabel.image = photoimage  # Сохраняем референс (вот это глупо сделано, как по-моему, но так надо)
 
-    def packFrames(self):
-        self.imageThumbnailFrame.pack(side='left') # зафигачиваем тамбнейлы слева
-        self.imagePreviewFrame.pack(side='right') # а превью справа
+    def pack_frames(self):
+        self.imageThumbnailFrame.pack(side='left')  # зафигачиваем тамбнейлы слева
+        self.imagePreviewFrame.pack(side='right')  # а превью справа
 
-    def makeImageLabels(self, window, imageFiles, size):
-        imageLabels = []
-        for file in imageFiles: # для каждого файла в списке файлов
-            image = Image.open(file) # открываем с помощью PIL
-            image.thumbnail(size, Image.ANTIALIAS) # уменьшаем
-            photoimage = ImageTk.PhotoImage(image) # конвертируем в формат TkInter
-            label = Label(window, image=photoimage) # Делаем, собственно, лейбл
-            label.image = photoimage #
+    def make_image_labels(self, window, image_files, size):
+        image_labels = []
+        for file in image_files:  # для каждого файла в списке файлов
+            image = Image.open(file)  # открываем с помощью PIL
+            image.thumbnail(size, Image.ANTIALIAS)  # уменьшаем
+            photoimage = ImageTk.PhotoImage(image)  # конвертируем в формат TkInter
+            label = Label(window, image=photoimage)  # Делаем, собственно, лейбл
+            label.image = photoimage  #
             # >You must keep a reference to the image object in your Python program, either by storing it in a global
             # >variable, or by attaching it to another object.
             # глупо сделали, ака "делай так и будет работать"
-            imageLabels.append(label) # запихиваем получившийся лейбл в imageLabels
-        return imageLabels # который и возвращаем
+            image_labels.append(label)  # запихиваем получившийся лейбл в image_labels
+        return image_labels  # который и возвращаем
 
-    def packImages(self, imageLabels):
+    def pack_images(self, image_labels):
         i = 0
 
-        for label in imageLabels:
-            label.pack() # запихиваеееем
+        for label in image_labels:
+            label.pack()  # запихиваеееем
             label.path = self.imageFilenames[i]
-            i = i+1
+            i += 1
             label.bind("<1>", self.on_main_click)
-        i = 0
-        
+
     def on_main_click(self, event):
         self.previewLabel.pack_forget()
         try:
@@ -105,23 +103,23 @@ class Viewer:
                 print('new imagelist:')
                 for text in self.imageFilenames:
                     print(text)
-                for label in self.imageThumbnailLabels:    
-                    label.pack_forget()
-                self.imageThumbnailLabels = self.makeImageLabels(self.imageThumbnailFrame, self.imageFilenames, self.thumbSize)
                 for label in self.imageThumbnailLabels:
-                    label.pack() # запихиваеееем
+                    label.pack_forget()
+                self.imageThumbnailLabels = self.make_image_labels(self.imageThumbnailFrame, self.imageFilenames,
+                                                                   self.thumbSize)
+                for label in self.imageThumbnailLabels:
+                    label.pack()  # запихиваеееем
 
-                self.packImages(self.imageThumbnailLabels)
-                pickle.dump(self.imageFilenames, open("order.pic","wb"))
+                self.pack_images(self.imageThumbnailLabels)
+                pickle.dump(self.imageFilenames, open("order.pic", "wb"))
 
-                
             except ValueError:
                 pass
-        self.setImagePreviewLabel(self.imagePreviewFrame, self.nowClick, self.previewSize)
+        self.set_image_preview_label(self.imagePreviewFrame, self.nowClick, self.previewSize)
         self.previewLabel.pack()
         self.previewLabel.bind("<1>", self.on_main_click)
 
-    def unpackThumbnails(self):
+    def unpack_thumbnails(self):
         for label in self.imageThumbnailLabels:
             label.pack_forget()
 
@@ -130,5 +128,4 @@ class Viewer:
 
     def run(self):
 
-        self.root.mainloop() # пошло выполнение программы
-
+        self.root.mainloop()  # пошло выполнение программы
